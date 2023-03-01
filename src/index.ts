59945a2132c97cbfe5e76b5b288d4dbd8d791b60
fran <franciscopischetz@gmail.com>
2023-02-28 21:16:44 -0300

    Add .env support and listen to env host and port

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-
+import * as dotenv from 'dotenv'
+dotenv.config()
 import app from './app'
 
+const HOST = process.env.HOST || '0.0.0.0'
+const PORT = parseInt(process.env.PORT) || 3001 
 
-app.listen(3001, () => console.log(`listening on port 3001`))
+app.listen(PORT,`${HOST}`, () => console.log(`listening host ${HOST} and port ${PORT}`))
